232cc8b7bbbd715c577a9c2e01aff74ee8e07b31
cakepower <32028127+cakepower@users.noreply.github.com>
2021-02-07 21:29:59 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,6 @@
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-    motorbit.freestyle(pins.map(
-    pins.analogReadPin(AnalogPin.P3),
-    0,
-    1023,
-    -100,
-    100
-    ), pins.map(
-    pins.analogReadPin(AnalogPin.P3),
-    0,
-    1023,
-    -100,
-    100
-    ))
     pins.servoWritePin(AnalogPin.P4, pins.map(
     pins.analogReadPin(AnalogPin.P3),
     0,
@@ -21,7 +8,13 @@ basic.forever(function () {
     -180,
     180
     ))
+    pins.servoWritePin(AnalogPin.P10, pins.map(
+    pins.analogReadPin(AnalogPin.P3),
+    0,
+    1023,
+    0,
+    180
+    ))
     serial.writeNumber(pins.analogReadPin(AnalogPin.P3))
     serial.writeLine("")
-    basic.pause(100)
 })
